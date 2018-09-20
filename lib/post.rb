e87f14fe47c00443169804209e1bbdd65074f53e
anavarreteuk <a.navarrete.uk@gmail.com>
2018-09-20 22:43:18 +0100

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -3,8 +3,4 @@ class Post
 
 attr_accessor :title, :author
 
-def initialize(title= "Tucu")
-
-end
-
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,6 @@
-require "pry"
+
 class Song
 
 attr_accessor :title, :artist
 
-def initialize(title= "Taca")
-
-end
-#binding.pry
 end
